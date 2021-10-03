4aa687eb8623bca7677ec18cee7e8aa3b27fb4b5
abarhub <abarhub@gmail.com>
2021-10-03 10:50:35 +0200

    utilisation de chemins relatifs

--- src/main/java/org/jvm2llvm/App.java
+++ src/main/java/org/jvm2llvm/App.java
@@ -28,7 +28,7 @@ public class App
 
 		read=new ReadFile();
 
-		res=read.read("D:\\projet\\jvm2llvm\\target\\classes\\org\\jvm2llvm\\Test1.class");
+		res=read.read("target\\classes\\org\\jvm2llvm\\Test1.class");
 
 		System.out.println("res="+res);
 
--- src/main/java/org/jvm2llvm/Tools.java
+++ src/main/java/org/jvm2llvm/Tools.java
@@ -24,8 +24,8 @@ public class Tools
         //System.out.println( "Hello World!" );
 	    //C:\Users\Alain\Documents\Visual Studio 2010\Projects\tjvm\java
 	    //
-	    f=new File("D:\\projet\\jvm2llvm\\doc\\tools\\opcodes3.csv");
-	    rep_out=new File("D:\\projet\\jvm2llvm\\src\\main\\java\\org\\jvm2llvm");
+	    f=new File("doc\\tools\\opcodes3.csv");
+	    rep_out=new File("src\\main\\java\\org\\jvm2llvm");
 	    list=read_csv(f);
 	    System.out.println("liste="+list);
 	    genere(list,rep_out);
